c0f27f6fac4de2830e646c7bad062edffe5357ce
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2025-01-03 22:10:00 -0300

    Initialize pizzashop API project with PostgreSQL setup, including Docker configuration, database schema, and environment management.

---
+++ drizzle/0000_blue_trish_tilby.sql
@@ -0,0 +1,11 @@
+CREATE TYPE "public"."user_role" AS ENUM('manager', 'customer');--> statement-breakpoint
+CREATE TABLE "user" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"phone" text,
+	"role" "user_role" DEFAULT 'customer' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
---
+++ src/http/server.ts
@@ -0,0 +1,8 @@
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+    .get("/", () => "Hello World")
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
