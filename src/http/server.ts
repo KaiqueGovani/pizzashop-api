9e5a408abe1191c81f7b2976a2695d9920eeec0e
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2025-01-17 22:50:00 -0300

    Implement POST endpoint for adding restaurants and managers in the server API

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,35 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import { db } from '../db/db';
+import { restaurants, users } from '../db/schema';
 
 const app = new Elysia()
-  .get('/', () => 'Hello World')
+  .post('/restaurants', async ({ body, set }) => {
+    const { restaurantName, name, email, phone  } = body;
+
+    const [manager] = await db.insert(users).values({
+      name,
+      email,
+      phone,
+      role: 'manager',
+    }).returning({
+      id: users.id,
+    })
+
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      managerId: manager.id,
+    })
+
+    set.status = 204
+  },
+{
+  body: t.Object({
+    restaurantName: t.String(),
+    name: t.String(),
+    email: t.String({ format: 'email' }),
+    phone: t.String(),
+  }),
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
